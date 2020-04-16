64a635488f1caa36a54344dda0b69628c09c8350
Lidia342 <lidiaabraham0@gmail.com>
2020-04-16 22:32:08 +0200

    bla

--- TestGit/src/sample/Hope.java
+++ TestGit/src/sample/Hope.java
@@ -1,6 +1,15 @@
 package sample;
 
 public class Hope {
-
-
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String userName;
+    private String password;
+    private String email;
+    private String mobile;
+    private String userRole;
+    private int paymentCard_id;
+    private int addresses_id;
 }
